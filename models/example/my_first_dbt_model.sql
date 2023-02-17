6071e3c7c5e9016e8f85505d4148504fd1f88e19
nour.kourdi@westnetz.de <nour.kourdi@westnetz.de>
2023-02-17 21:22:55 +0000

    add a new try

--- models/example/my_first_dbt_model.sql
+++ models/example/my_first_dbt_model.sql
@@ -19,11 +19,11 @@ with
     )
 
 
-select *
+select * from source_data
+
 
 
 /*
     Uncomment the line below to remove records with null `id` values
 */
 -- where id is not null
-from source_data
