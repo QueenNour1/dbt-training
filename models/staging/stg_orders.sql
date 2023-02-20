29bf662f5d51651b51622d178932fdf8a52aaf23
nour.kourdi@westnetz.de <nour.kourdi@westnetz.de>
2023-02-20 12:10:25 +0000

    added delivery team and added it to stg order

--- models/staging/stg_orders.sql
+++ models/staging/stg_orders.sql
@@ -15,10 +15,12 @@ p.productid,
 p.category,
 p.productidname ,
 p.subcategory,
-{{ markup('ORDERSELLINGPRISE', 'ORDERCOST') }} as markup
+{{ markup('ORDERSELLINGPRISE', 'ORDERCOST') }} as markup, 
+d.delivery_team
 from {{ ref('raw_orders') }} as o
 left join {{ ref('raw_customer') }} as c
 on o.customerid = c.customerid
 left join {{ ref('raw_product') }} as p
 on o.productid = p.productid
-
+left join {{ ref('delivery_team') }} as d 
+on o.shipmode = d.shipmode
